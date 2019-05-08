8eeecd2563b023e265122c06c26ca6def08736e1
Alex <alejandro.munoz@student.uts.edu.au>
2019-05-09 09:11:29 +1000

    Made camera script so camera follows player smoothly

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+
+    public Transform target;
+
+    //Smaller = smoother
+    public float smoothSpeed = 4f;
+    public Vector3 offset;
+
+    void Start()
+    {
+        
+    }
+
+    //Similar to update, except run straight after
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        transform.position = smoothedPosition;
+    }
+}
