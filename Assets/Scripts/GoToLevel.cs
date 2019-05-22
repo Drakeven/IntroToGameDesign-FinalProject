b9f02664630d50e8a772fbfa6dc386f4624aed44
zenizidizici <0zen0.0ran0@gmail.com>
2019-05-22 12:14:39 +1000

    Added Hub and basic functionalities
    Hub:
    - Teleport
    - Change scene

---
+++ Assets/Scripts/GoToLevel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToLevel : MonoBehaviour
+{
+    public KeyCode interactKey; //key to interact
+    public GameObject ui; //on screen text
+    public string level; //level to be changed to
+    public GameObject player; //player
+
+    void OnTriggerStay2D(Collider2D other) //while the player is in the trigger
+    {
+        ui.SetActive(true); //text is displayed 
+        if (Input.GetKeyDown(interactKey)) //if player interacts
+        {
+            SceneManager.LoadScene(level); //change scene
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision) //when player leaves trigger
+    {
+        ui.SetActive(false); //text is hidden
+    }
+}
---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public KeyCode interactKey;
+    public GameObject ui;
+    public Transform teleportLocation;
+    public GameObject player;
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        ui.SetActive(true);
+        if (Input.GetKeyDown(interactKey))
+        {
+            player.transform.position = teleportLocation.transform.position; //player position changes to teleportLocation positions
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        ui.SetActive(false);
+    }
+}
